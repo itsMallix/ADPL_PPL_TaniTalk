4b00b3b8b07d1348404fcb92efd605fdc602dc4b
itsMallix <amandamaulana00@gmail.com>
2023-04-19 12:46:39 +0700

    next

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TaniTalk</title>
 </head>
 <body>
-    
+    <?php
+    if(isset($_GET['pesan'])){
+        if($_GET['pesan']=='gagal'){
+            echo "<div class='alert'>Username dan Password Salah !</div>";
+        }
+    }
+    ?>
+
+    <div class="box">
+        <p class="text_login">Silahkan Login</p>
+        <form action=""></form>
+    </div>
 </body>
 </html>
