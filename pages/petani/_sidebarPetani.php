94dbd3f226855daaa962efb8946039612fd71d68
itsMallix <amandamaulana00@gmail.com>
2023-04-25 02:13:58 +0700

    add cuaca otw1

--- pages/petani/_sidebarPetani.php
+++ pages/petani/_sidebarPetani.php
@@ -4,7 +4,7 @@
         <li><a href="dashboardPetani.php">Dashboard</a></li>
         <li><a href="identifikasiPenyakit.php">Identifikasi Penyakit</a></li>
         <li><a href="kalkulatorPupuk.php">Kalkulasi pupuk</a></li>
-        <li><a href="#">Prakiraan Cuaca</a></li>
+        <li><a href="cuacaPetani.php">Prakiraan Cuaca</a></li>
         <li><a href="#">Pencatatan</a></li>
     </ul>
 </div>
--- pages/petani/cuacaPetani.php
+++ pages/petani/cuacaPetani.php
@@ -6,7 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuaca</title>
 </head>
+<?php
+    include '_sidebarPetani.php';
+    include '_navbarPetani.php';
+?>
 <body>
-    
+    <div class="container">
+        <h2 class="title">Prakiraan Cuaca</h2>
+        <div class="input-grup">
+            <input type="text" class="formControl" id="city">
+            <button class="searchBtn" id="search">Search</button>
+        </div>
+    </div>
 </body>
 </html>
+
+<style>
+    .container{
+        padding: 5px;
+        margin-left: 320px;
+        margin-right: 320px;
+    }
+    .searchBtn{
+        border: none;
+        background-color: blueviolet;
+    }
+</style>
