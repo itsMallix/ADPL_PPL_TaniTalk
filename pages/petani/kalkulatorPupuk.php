3799936f95f26b1bc59cec273ffbfe928b684708
itsMallix <amandamaulana00@gmail.com>
2023-04-25 02:01:50 +0700

    add cuaca otw

--- pages/petani/kalkulatorPupuk.php
+++ pages/petani/kalkulatorPupuk.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/kalkulasiPupuk.css">
     <title>Kalkulator Pupuk</title>
 </head>
     <?php
@@ -11,10 +12,15 @@
         include '_navbarPetani.php';
 
         if(isset($_POST['hitung'])){
+            $dosis = 0.33;
             if($_POST['jenisPola']=='persegi'){
-                $dosis = 0.33;
                 $jumlah_tanaman = ($_POST['luasLahan']/$_POST['jarakTanam_1']) * ($_POST['luasLahan']/$_POST['jarakTanam_2']);
-                // $jumlah_tanaman = ($luas_lahan/$jarak_1) * ($luas_lahan/$jarak_2);
+                $hasil = $jumlah_tanaman * $dosis;
+            }elseif($_POST['jenisPola']=='segitiga'){
+                $jumlah_tanaman = ($_POST['luasLahan']/($_POST['jarakTanam_1']+$_POST['jarakTanam_2']))*2;
+                $hasil = $jumlah_tanaman * $dosis;
+            }elseif($_POST['jenisPola']=='persegi_P'){
+                $jumlah_tanaman = ($_POST['luasLahan']/($_POST['jarakTanam_1']*$_POST['jarakTanam_2']));
                 $hasil = $jumlah_tanaman * $dosis;
             }
         }else{
@@ -26,7 +32,7 @@
         <div class="kalkulator">
             <form action="" method="POST">
                 <h2>Kalkulator Pupuk</h2>
-                <input type="text" name="luasLahan" placeholder="Luas Lahan" class="input">
+                <input type="text" name="luasLahan" placeholder="Luas Lahan/m2" class="input">
 
                 <label>Pola Tanam</label>
                 <select name="jenisPola" class="jenis">
@@ -34,8 +40,8 @@
                     <option value="segitiga">Segitiga</option>
                     <option value="persegi_P">Persegi Panjang</option>
                 </select>
-                <input type="text" name="jarakTanam_1" placeholder="Jarak Tanam Sisi 1" class="input">
-                <input type="text" name="jarakTanam_2" placeholder="jarak Tanam Sisi 2" class="input">
+                <input type="text" name="jarakTanam_1" placeholder="Jarak Tanam Sisi 1/m" class="input">
+                <input type="text" name="jarakTanam_2" placeholder="jarak Tanam Sisi 2/m" class="input">
 
                 <input type="submit" value="Hitung" name="hitung" class="hitung">
                 <h2>Hasil : <?php echo $hasil ?> gram</h2>
@@ -44,42 +50,3 @@
     </div>
 </body>
 </html>
-
-<style>
-    .input{
-        width: 300px;
-        margin: 5px;
-        border: 1px solid;
-        border-radius: 5px;
-        padding: 10px;
-        font-size: 15pt;
-    }
-    .hitung{
-        border-top: none;
-        border-left: none;
-        border-right: none;
-        border-radius: 5px;
-        padding: 10px 20px;
-        border-bottom: 5px;
-        width: 300px;
-    }
-    .jenis{
-        font-size: 12pt;
-        border: 1px solid;
-        width: 220px;
-        margin: 5px;
-        border-radius: 5px;
-        padding: 10px;
-    }
-    .content{
-        margin-left: 320px;
-        width: 350px;
-        margin: 100px auto;
-        padding: 10px 20px 50px 20px;
-        border-radius: 5px;
-        box-shadow: 0px 10px 20px 0px;
-    }
-    .kalkulator label{
-        font-size: 12pt;
-    }
-</style>
