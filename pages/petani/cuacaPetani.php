dd98a225b3acdd6c864c1847755f4493af14df4a
itsMallix <amandamaulana00@gmail.com>
2023-04-26 16:20:52 +0700

    update cuaca

--- pages/petani/cuacaPetani.php
+++ pages/petani/cuacaPetani.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/cuaca.css">
     <title>Cuaca</title>
 </head>
 <?php
@@ -14,21 +15,9 @@
     <div class="container">
         <h2 class="title">Prakiraan Cuaca</h2>
         <div class="input-grup">
-            <input type="text" class="formControl" id="city">
+            <input type="text" class="formControl" id="city" placeholder="Masukkan Nama Kota">
             <button class="searchBtn" id="search">Search</button>
         </div>
     </div>
 </body>
 </html>
-
-<style>
-    .container{
-        padding: 5px;
-        margin-left: 320px;
-        margin-right: 320px;
-    }
-    .searchBtn{
-        border: none;
-        background-color: blueviolet;
-    }
-</style>
